120a61e30c23b073b03889a5ccef29a56f70932f
dPerez015 <davidperezgallego@enti.cat>
2017-11-11 17:58:57 +0100

    escena menu

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ var bomberman = bomberman || {};
 
 bomberman.game = new Phaser.Game(gameOptions.gameWidth,gameOptions.gameHeight,Phaser.AUTO,null,this,false,false);
 
-//bomberman.game.state.add('menu',bomberman.menu);
-//bomberman.game.state.start('menu');
+bomberman.game.state.add('menu',bomberman.menu);
+bomberman.game.state.start('menu');
---
+++ js/menu.js
@@ -0,0 +1,21 @@
+var bomberman = bomberman || {};
+
+bomberman.menu = {
+    
+    init:function(){
+        
+    },
+    
+    preload:function(){
+        
+    },
+    
+    create:function(){
+
+    },
+    
+    update:function(){
+        
+    },   
+    
+}
