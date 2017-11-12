2478eed4b009998b1c2c28c362bbb8ab5becc5ad
Marc Ruiz <ruizmp27@gmail.com>
2017-11-12 23:09:54 +0100

    niños pollers

--- js/level.js
+++ js/level.js
@@ -22,7 +22,7 @@ bomberman.level = {
         this.space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         this.esc = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
         
-        this.puff = new platformer.puffPuff(game,0,0,3,'down',this, 1, 100);
+        this.puff = new bomberman.puffPuff(this.game,0,0,3,'down',this, 1, 100);
         this.game.add.existing(this.puff);
     },
     
