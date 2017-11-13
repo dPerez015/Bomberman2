fdc675385a50f1c07121db31e14020e4710bf257
dPerez015 <davidperezgallego@enti.cat>
2017-11-13 15:52:49 +0100

    lvl1-1

---
+++ maps/Mundo1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="world1" tilewidth="16" tileheight="16" tilecount="130" columns="13">
+ <image source="../img/world1.png" width="208" height="160"/>
+</tileset>
