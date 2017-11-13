bedd8c57f7f9b85d3b1d1e03ceaa09219f902107
david lopez saludes <davidlopez1972@hotmail.es>
2017-11-14 00:18:09 +0100

    animacions ajustades

--- js/bomberman_prefab.js
+++ js/bomberman_prefab.js
@@ -7,21 +7,21 @@ bomberman.bomberman_prefab = function(game, x, y, _currLevel, _speed, _life, _wi
   //this.scale.setTo(2);
   //Hauriem de mirar si hi ha alguna manera de fer una funció que retorni l'animació
   this.animations.add('walk_up',[0,1,2],10,true);
-  this.animations.add('walk_left',[3,4,5],10,true);
-  this.animations.add('walk_down',[6,7,8],10,true);
-  this.animations.add('walk_right',[9,10,11],10,true);
-  this.animations.add('ready_fight',[12],1,true);
-  this.animations.add('placing_bomb_up',[13,14,15,16],10,true);
-  this.animations.add('placing_bomb_right',[17,18,19,20],10,true);
-  this.animations.add('placing_bomb_down',[21,22,23],10,true);
-  this.animations.add('placing_bomb_left',[24,25,26,27],10,true);
+  this.animations.add('walk_left',[10,11,12],10,true);
+  this.animations.add('walk_down',[20,21,22],10,true);
+  this.animations.add('walk_right',[30,31,32],10,true);
+  this.animations.add('ready_fight',[33],1,true);
+  this.animations.add('placing_bomb_up',[40,41,42,43],10,true);
+  this.animations.add('placing_bomb_right',[50,51,52,53],10,true);
+  this.animations.add('placing_bomb_down',[60,61,62],10,true);
+  this.animations.add('placing_bomb_left',[70,71,72,73],10,true);
   /*this.animations.add('throw_bomb_up',[28,29],10,true);
   this.animations.add('throw_bomb_left',[30,31],10,true);
   this.animations.add('throw_bomb_down',[32,33],10,true);
   this.animations.add('throw_bomb_right',[34,35],10,true);*/
-  this.animations.add('damage_before_dead',[36,37,38,39],10,true);
-  this.animations.add('dead',[40,41,42,43,44],10,true);
-  this.animations.add('win',[129,120,131],10,true);
+  this.animations.add('damage_before_dead',[100,101,102,103],10,true);
+  this.animations.add('dead',[110,111,112,113,114],10,true);
+  this.animations.add('win',[270,271,272],10,true);
   //this.game.physics.arcade.enable(this);
   this.posX = x;
   this.posY = y;
