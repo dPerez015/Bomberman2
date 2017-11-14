df7940f6a07f23c11428249532e21e3d42f38484
david lopez saludes <davidlopez1972@hotmail.es>
2017-11-14 02:35:34 +0100

    Explosion

--- js/explosion_prefab.js
+++ js/explosion_prefab.js
@@ -9,7 +9,7 @@ bomberman.explosion_prefab = function(game,x,y){
     this.animations.add('Vertical_explosion', [4,12,20,28,36], 10, true);
     this.animations.add('Horizontal_explosion', [5,13,21,29,37], 10, true);
     this.animations.add('Center_explosion', [6,14,22,30,38], 10, true);
-    this.animations.add('Upper_explosion', [7,15,23,31,39], 10, true);
+    this.animations.add('Objects_explosion', [7,15,23,31,39], 10, true);
 
 }
 
