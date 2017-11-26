ef6075afdf766c2dc01d4975bf78c04f8693f953
david lopez saludes <davidlopez1972@hotmail.es>
2017-11-26 14:38:48 +0100

    No entenc pq no funciona si a la web i als exemples de phaser diu que és així

--- js/menu.js
+++ js/menu.js
@@ -16,16 +16,17 @@ bomberman.menu = {
         
         this.load.audio('Menu_music', 'sound/Menu_soundtrack.mp3');
         
-        this.hasToPlay = true;
+
     },
     
     create:function(){
         this.ProvMenu = this.game.add.image(0, 0, 'ProvMenu'); 
         
-        //this.ProvButton = this.game.add.button(this.world.centerX, this.world.centerY, 'ProvButton', this.gameStart, this);
-        //this.ProvButton.anchor.setTo(.5);
-        //this.ProvButton.scale.setTo(.5);
-        this.startGameButton = new bomberman.MenuButton_prefab(this.game,287,437,'Button',this.gameStart);
+        this.music = this.add.audio('Menu_music');
+        this.music.play();
+        this.music.loopFull(0.1);
+        
+        this.startGameButton = new bomberman.MenuButton_prefab(this.game,287,437,'Button',this.gameStart,);
         this.game.add.existing(this.startGameButton);
         
         this.battleModeButton = new bomberman.MenuButton_prefab(this.game,287,487,'Button',this.battleMode);
@@ -34,29 +35,26 @@ bomberman.menu = {
         this.passwordButton = new bomberman.MenuButton_prefab(this.game,287,537,'Button',this.passordsMode);
         this.game.add.existing(this.passwordButton);
         
-        this.music = this.add.audio('Menu_music');
-        this.music.play();
-        this.music.loopFull(0.1);
+
     },
     
     update:function(){
-        if(this.hasToPlay == false){
-            this.music.pause();
-        }
-    },   
+
+    },
     gameStart:function(){
+        this.music.destroy();
         bomberman.loadScene('main');
-        this.hasToPlay = false;
     },
     battleMode:function(){
         //cosas
+        this.music.destroy();
         bomberman.loadScene('comsoon');
-        this.hasToPlay = false;
     },
     passordsMode:function(){
         //radev rules
+        this.music.destroy();
         bomberman.loadScene('comsoon');
-        this.hasToPlay = false;
+        
     }
     
 }
