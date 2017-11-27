a34da5a7f6eefdaee25d628f324b46ce51c5e83c
david lopez saludes <davidlopez1972@hotmail.es>
2017-11-27 17:43:26 +0100

    Switch explosion implemented

--- js/explosion_prefab.js
+++ js/explosion_prefab.js
@@ -1,24 +1,29 @@
 var bomberman = bomberman || {};
 
-bomberman.explosion_prefab = function(game,x,y, _hasExploted, _timer){
+bomberman.explosion_prefab = function(game,x,y, type){
     Phaser.Sprite.call(this, game, x, y, 'explosions');
-    game.add.existing(this);
-    this.animations.add('Upper_explosion', [0,8,16,24,32], 10, true);
-    this.animations.add('Down_explosion', [1,9,17,25,33], 10, true);
-    this.animations.add('Right_explosion', [2,10,18,26,34], 10, true);
-    this.animations.add('Left_explosion', [3,11,19,27,35], 10, true);
-    this.animations.add('Vertical_explosion', [4,12,20,28,36], 10, true);
-    this.animations.add('Horizontal_explosion', [5,13,21,29,37], 10, true);
-    this.animations.add('Center_explosion', [6,14,22,30,38], 10, true);
-    this.animations.add('PowerUp_explosion', [7,15,23,31,39], 10, true);
-    this.isExploding = _hasExploted;
-    this.posX = x;
-    this.posY = y;
-    this.rightExplodes = x;
-    this.leftExplodes = x;
-    this.upExplodes = y;
-    this.downExplodes = y;
-    this.timer = _timer;
+
+    switch (type){
+        case 0:
+           this.animations.add('Center_explosion', [6,14,22,30,38], 10, true);
+        case 1:
+            this.animations.add('Upper_explosion', [0,8,16,24,32], 10, true);
+        case 2:
+            this.animations.add('Down_explosion', [1,9,17,25,33], 10, true);
+        case 3:
+            this.animations.add('Right_explosion', [2,10,18,26,34], 10, true);
+        case 4:
+            this.animations.add('Left_explosion', [3,11,19,27,35], 10, true);
+        case 5:
+            this.animations.add('Vertical_explosion', [4,12,20,28,36], 10, true);
+        case 6:
+            this.animations.add('Horizontal_explosion', [5,13,21,29,37], 10, true);
+        case 7:
+            this.animations.add('PowerUp_explosion', [7,15,23,31,39], 10, true);
+        default:
+            break;
+                };
+
 };
 
 bomberman.explosion_prefab.prototype.update = function(){
