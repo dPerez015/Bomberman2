6943a200294fa7e90c62966082bb055bef9a3b0d
david lopez saludes <davidlopez1972@hotmail.es>
2017-11-29 00:10:38 +0100

    Transition ended

--- js/Transition_scene.js
+++ js/Transition_scene.js
@@ -1,21 +1,28 @@
 var bomberman = bomberman || {};
 
+var music;
+
 bomberman.Transicion_scene = {
     
     preload:function(){
         this.load.image('lvl1-1', 'img/scene1-1.png');
+        this.load.audio('Stage_change_music', 'sound/Stage-Start_soundtrack.mp3');
         
     },
     create:function(){
         this.mainImage = this.add.image(this.game.world.centerX, this.game.world.centerY, 'lvl1-1');
         this.mainImage.scale.setTo(0.5);
         this.mainImage.anchor.setTo(0.5);
-        this.timer = 100;
+        this.timer = 200;
+        
+        music = this.game.add.audio('Stage_change_music');
+        music.play();
     },
     update:function(){
         if(this.timer != 0){
             this.timer--;
             if(this.timer == 0){
+                music.stop();
                 bomberman.loadScene("main");
             }
         }
