3b83a4a280d5b993887f0b6709c6853b330b2f89
david lopez saludes <davidlopez1972@hotmail.es>
2017-12-14 20:07:55 +0100

    Odio mi vida

---
+++ maps/Mundo2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Mundo2" tilewidth="16" tileheight="16" tilecount="130" columns="13">
+ <image source="../img/Mundo2.png" width="208" height="160"/>
+</tileset>
