ca5cc0b56c5d6bc3f5ec7569827ac8410f378525
david lopez saludes <davidlopez1972@hotmail.es>
2017-12-14 19:14:11 +0100

    Mapa 3 DONE

---
+++ maps/Mundo 1-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Mundo2" tilewidth="16" tileheight="16" tilecount="160" columns="16">
+ <image source="../img/Mundo2.png" width="256" height="160"/>
+</tileset>
--- maps/Mundo1-2.tsx
+++ maps/Mundo1-2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="unnamed" tilewidth="16" tileheight="16" tilecount="160" columns="16">
- <image source="../img/unnamed.png" width="256" height="160"/>
+<tileset name="Mundo2" tilewidth="16" tileheight="16" tilecount="130" columns="13">
+ <image source="../img/Mundo2.png" width="208" height="160"/>
 </tileset>
