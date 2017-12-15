ea4e2a9cf517f27daf73de814837c2b8402e4612
david lopez saludes <davidlopez1972@hotmail.es>
2017-12-15 13:51:19 +0100

    lvl1-2 no entenc pq no es fa bé, lvl1-3 perfecte

--- maps/Boss1Map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Boss_Map" tilewidth="32" tileheight="32" tilecount="6" columns="2">
- <image source="../img/Boss_Map.png" width="64" height="96"/>
-</tileset>
