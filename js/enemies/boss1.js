c37c601307af7811a2cdd28d88bdb6ead06982fb
david lopez saludes <davidlopez1972@hotmail.es>
2018-01-04 00:13:29 +0100

    Varies coses afegides del boss, s'han d'afegir coses noves, no crec que em porti massa

--- js/enemies/boss1.js
+++ js/enemies/boss1.js
@@ -24,9 +24,31 @@ bomberman.boss1 = function(game, x, y, speed, level, direction){
     this.hp = 12;
     this.score = gameValues.boss1Score;
     this.isHitting = false;
+    this.isRight = false;
     
     this.game.physics.arcade.enable(this);
     this.body.velocity.x = this.speed;
+    
+    this.checkPlayer=function(){
+    var posX=this.level.bg.getTileX(this.body.x);
+        
+    var playerPosX=this.level.bg.getTileX(this.level.player.body.position.x);
+    if(playerPosX==posX){
+        var canPass=true;
+        var difX=playerPosX-posX;
+        var dif=difX;
+        if(dif!=0){
+            difX/=Math.abs(difX);
+        }
+        
+        for(var i=0; i<=dif ;i++){
+            posX+=difX;
+        }
+        return canPass;
+    }
+        else
+            return false;
+    }
 };
 
 bomberman.boss1.prototype = Object.create(Phaser.Sprite.prototype);
@@ -34,34 +56,74 @@ bomberman.boss1.prototype.constructor = bomberman.boss1;
 
 bomberman.boss1.prototype.update = function(){
     this.game.physics.arcade.collide(this,this.level.walls);
+
+    this.game.physics.arcade.overlap(this, this.level.bombas,this.level.player.bombas.kill,null,this);
+    
+    this.game.physics.arcade.collide(this,this.level.player,this.level.player.bombermanHit,null,this);
+    
+    this.game.physics.arcade.overlap(this,this.level.explosions,this.hit,null,this);
+    
+    if(tthis.body.touching.right && this.isRight == true){
+        this.isRight = false;
+        this.changeDirection();
+    }else if(this.body.touching.left && this.isRight == false){
+        this.isRight = true;
+        this.changeDirection();
+    }
+    
     //Per comprobar pos del player mirar magnethelm
-    if(this.game.physics.arcade.overlap(this, this.level.bombas)){
-        this.level.player.bombas.kill();//No es pot fer, mirar el codi del boto de level o el choquemuro
+    if(!this.isHitting){
+    this.timeSinceLastAtract+=this.level.player.game.time.physicsElapsed;
+    //movimiento
+    if(this.checkMovement()){
+            
+        this.changeDirection();
     }
-    
-    if(this.game.physics.arcade.collide(this,this.level.player)){
-        this.level.player.bombermanHit();
+    //comprovar si ha de atraer
+    if(Math.floor(this.body.position.x)%16==0 && Math.floor(this.body.position.y)%16==0 && this.timeSinceLastAtract>this.waitTime){
+        if(this.checkPlayer()){
+            this.isHitting=true;
+            this.animations.play('punchLeft');
+            this.body.velocity.setTo(0,0);
+            this.timeAtracting=0;
+        }
     }
-    
-    if(this.game.physics.arcade.overlap(this,this.level.explosions)){
-        this.hit()
+        
     }
-    
-    if(this.body.y != this.level.player.y && this.hp >=10){
-        this.animations.play('standStill');
-        this.body.velocity.x -= this.speed;
-    }else if(this.body.y == this.player.y && this.hp >=10){
-        this.animations.play('punchLeft');
-    }
-    if(this.body.y != this.level.player.y && this.hp >=7 && this.hp <= 9){
-        this.animations.play('standStillWeak');
-        this.body.velocity
+    else{ //atraccion
+        if(this.checkPlayer()){
+            var posX=this.level.bg.getTileX(this.body.x);
+            var posY=this.level.bg.getTileY(this.body.y);
+        
+            var playerPosX=this.level.bg.getTileX(this.level.player.body.position.x);
+            var playerPosY=this.level.bg.getTileY(this.level.player.body.position.y);
+            
+            var vX=posX-playerPosX;
+            var vY=posY-playerPosY;
+            
+            if(vX!=0)
+                vX=vX/Math.abs(vX);
+            if(vY!=0)
+                vY=vY/Math.abs(vY);
+            
+            
+            this.level.player.body.velocity.x+=(vX*this.playerAtractingVel);
+            this.level.player.body.velocity.y+=(vY*this.playerAtractingVel);
+            
+        }
+        this.timeAtracting+=this.level.player.game.time.physicsElapsed;
+            
+        if(this.timeAtracting>this.timeToAtract){
+                this.isHitting=false;
+                this.changeSpeed();
+                this.timeSinceLastAtract=0;
+            }
+        
     }
     
 };
 
 bomberman.boss1.prototype.changeDirection = function(){
-    var arrDir = ['left', 'right'];
     if(this.hp >= 10 && this.hp <= 12){
         this.animations.play('standStill');
     }
@@ -74,7 +136,16 @@ bomberman.boss1.prototype.changeDirection = function(){
     if(this.hp >= 1 && this.hp <= 3){
         this.animations.play('standStillAlmostDead');
     }
-    
+    switch(this.isRight){
+        case true:
+            this.body.velocity.x += this.speed;
+            this.body.velocity.y = 0;
+            break;
+        case false:
+            this.body.velocity.x -= this.speed;
+            this.body.velocity.y = 0;
+            break;
+    }
     
 };
 
